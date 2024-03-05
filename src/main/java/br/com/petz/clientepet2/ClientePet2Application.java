ed9d7fc206c4d7aecb287c30072d62cbd99af0a2
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-03-05 12:16:33 -0300

    implementa Api de teste

--- src/main/java/br/com/petz/clientepet2/ClientePet2Application.java
+++ src/main/java/br/com/petz/clientepet2/ClientePet2Application.java
@@ -2,10 +2,19 @@ package br.com.petz.clientepet2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePet2Application {
-
+	
+	@GetMapping
+	public String getHomeTeste() {
+		return "cliente pet - API HOME";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePet2Application.class, args);
 	}
