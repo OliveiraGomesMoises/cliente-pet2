63cd5c67cb7f54d9fd5ae4ac9fcebaf17c13f6ae
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-03-08 08:18:05 -0300

    implementa contrato Api

--- src/main/java/br/com/petz/clientepet2/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet2/cliente/application/api/ClienteAPI.java
@@ -2,6 +2,7 @@ package br.com.petz.clientepet2.cliente.application.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public interface ClienteAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	 ClienteRespose postCliente(ClienteRequeste clienteRequeste);
+	 ClienteRespose postCliente(@RequestBody ClienteRequeste clienteRequeste);
 
 }
