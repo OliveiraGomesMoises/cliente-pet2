691aa0a9094bb958df9618272865a22e4430d00d
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-03-14 09:16:15 -0300

    implementa fluxo repository,teste ok

--- src/main/java/br/com/petz/clientepet2/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet2/cliente/application/service/ClienteApplicationService.java
@@ -31,7 +31,7 @@ public class ClienteApplicationService implements ClienteService {
 		log.info("[inicia] ClienteApplicationService - buscaTodosClientes");
 		List<Cliente> clientes = clienteRepository.buscaTodosClientes();
 		log.info("[finaliza] ClienteApplicationService - buscaTodosClientes");
-		return null;
+		return ClienteListResponse.converte(clientes);
 	}
 
 }
